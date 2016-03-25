01065f58bf811db41dbfed5638e830514e74505c
kapicaai <kapitsa-sashulchik@yandex.ru>
2016-03-25 11:51:43 +0300

    firstLoad

---
+++ Radio/Song.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+//ninject
+namespace MusicCollection
+{
+    [Serializable]
+    public partial class Song
+    {
+        public string Artist { get; set; }
+        public string Title { get; set; } 
+        public string Album { get; set; }
+        public string Year { get; set; }
+        public List<string> Tags { get; set; }
+        public string SongPath { get; set; }
+
+        [XmlAttribute]
+        private int rating;
+        TimeSpan longness;
+
+        public Song()
+        {
+            Tags = new List<string>();
+            Year = "1990";
+            rating = 0;
+        }
+
+        public static IComparer SortByArtist()
+        {
+            return (IComparer)new SortByArtistHelper(); 
+        }
+
+        public static IComparer SortByTitle()
+        {
+            return (IComparer)new SortByTitleHelper();
+        }
+
+        public static IComparer SortByYear()
+        {
+            return (IComparer)new SortByArtistHelper();
+        }
+        public static IComparer SortByRating()
+        {
+            return (IComparer)new SortByRatingHelper();
+        }
+    }
+
+    [Serializable]
+    public class SongCollection
+    {
+        public List<Song> collection;
+        public string name;
+        public string description;
+
+        public SongCollection()
+        {
+            collection = new List<Song>();
+        }
+
+        public SongCollection(string name) : this()
+        {
+            this.name = name;
+        }
+        
+        
+         
+    }
+
+    public class SongCollectFile
+    {
+        public bool ReadFromFile(string fileName, ref SongCollection rezult)
+        {
+            XmlSerializer xReader = new XmlSerializer(typeof(SongCollection));
+            try
+            {
+                rezult = (SongCollection)xReader.Deserialize(new StreamReader(fileName));
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+         
+        public bool WriteToFile(SongCollection songCollect, string fileName)
+        {
+            XmlSerializer xWriter = new XmlSerializer(typeof(SongCollection));
+            try
+            {
+                xWriter.Serialize(new StreamWriter(fileName), songCollect);
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Radio/SongComparersDeleg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicCollection;
+using System.Collections;
+
+namespace MusicCollection
+{
+    public partial class Song
+    {
+        private class SortByArtistHelper : IComparer
+        {
+            int IComparer.Compare(object a, object b)
+            {
+                Song s1 = (Song)a;
+                Song s2 = (Song)b;
+                return s1.Artist.CompareTo(s2.Artist);
+            }
+        }
+
+        private class SortByTitleHelper : IComparer
+        {
+            int IComparer.Compare(object x, object y)
+            {
+                return (x as Song).Title.CompareTo((y as Song).Title);
+            }
+        }
+
+        private class SortByYearHelper : IComparer
+        {
+            int IComparer.Compare(object x, object y)
+            {
+                return (x as Song).Year.CompareTo((y as Song).Year);
+            }
+        }
+
+        private class SortByRatingHelper : IComparer
+        {
+            int IComparer.Compare(object x, object y)
+            {
+                return (x as Song).rating.CompareTo((y as Song).rating);
+            }
+        }
+    }
+}
