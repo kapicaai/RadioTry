8bba319db312db3e8b8406d5caec765f36ae2ca7
kapicaai <kapitsa-sashulchik@yandex.ru>
2016-04-11 14:24:36 +0300

    Fourth time. data structure added

---
+++ Radio/ILoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCollection
+{
+
+    interface ILoader<T>
+    {
+        bool Upload(object resourse, T collectionToUpload);
+        bool Download(object resource, ref T collectionToUpload);
+    }
+}
